56d6f611fe4abea3fa8863952e4f7e42a0c470ad
Leon <irakaramale@gmail.com>
2026-08-05 18:05:23 +0300

    Add sign up page

---
+++ app/auth/sign-up/page.tsx
@@ -0,0 +1,11 @@
+import { SignUpForm } from "@/components/platform/auth/SignUpForm";
+
+function page() {
+  return (
+    <div className="flex w-full justify-center">
+      <SignUpForm />
+    </div>
+  );
+}
+
+export default page;
