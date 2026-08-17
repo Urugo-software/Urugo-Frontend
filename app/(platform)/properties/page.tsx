2ff66a962b062d189a4833c168c4eb64ade00f94
Leon <irakaramale@gmail.com>
2026-08-17 14:44:29 +0300

    style(properties): adjust property page layout formatting

--- app/(platform)/properties/page.tsx
+++ app/(platform)/properties/page.tsx
@@ -5,7 +5,7 @@ import RightSide from "@/components/platform/properties/RightSide";
 function page() {
   return (
     <section className=" w-full min-h-screen ">
-      <SectionContainer className="max-w-7xl  mx-auto py-10 lg:py-18 max-md:flex-col flex gap-6">
+      <SectionContainer className="max-w-7xl  mx-auto py-10 lg:py-18 max-md:flex-col flex gap-6 ">
         {/* left side */}
         <LeftSide />
         {/* right side */}
--- components/platform/properties/SearchInput.tsx
+++ components/platform/properties/SearchInput.tsx
@@ -5,16 +5,15 @@ interface SearchInputProps {
 
 function SearchInput({ className }: SearchInputProps) {
   return (
-     <div className="flex items-center border border-line rounded-xl hover:border-brand hover:ring-brand/60 hover:ring-2 transition-all duration-300  ">
-        <Search size={16} className="text-faint ml-3" />
-        <input
-          type="text"
-          placeholder="Search by location, e.g. Kimironko…"
-          className={`${className}`}
-        />
-      </div>
-  )
-  ;
+    <div className="flex items-center border border-line  hover:border-brand hover:ring-brand/60 hover:ring-2 transition-all duration-300  ">
+      <Search size={16} className="text-faint ml-3" />
+      <input
+        type="text"
+        placeholder="Search by location, e.g. Kimironko…"
+        className={`${className}`}
+      />
+    </div>
+  );
 }
 
 export default SearchInput;
