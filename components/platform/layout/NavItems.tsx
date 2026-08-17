272fe9b5a1935218377a2615bc507c968bf0a024
Leon <irakaramale@gmail.com>
2026-08-17 14:47:09 +0300

    refactor(layout): update navbar and nav items

--- components/platform/layout/NavItems.tsx
+++ components/platform/layout/NavItems.tsx
@@ -1,3 +1,6 @@
+"use client";
+import { usePathname } from "next/navigation";
+import { isActivePath } from "@/lib/utils";
 import Link from "next/link";
 
 interface NavItemsProps {
@@ -6,9 +9,12 @@ interface NavItemsProps {
 }
 
 function NavItems({ href, name }: NavItemsProps) {
+  const pathname = usePathname();
+  const isActive = isActivePath(pathname, href);
+  console.log("isActive", isActive, pathname, href);
   return (
     <Link
-      className="text-ink hover:border-b-2 duration-300 hover:border-brand/80 hover:text-brand rounded-full px-2 text-sm tracking-wide"
+      className={` hover:border-b-2 duration-300 hover:border-brand/80 hover:text-brand rounded-full px-2 text-sm tracking-wide ${isActive ? "border-b-2 border-brand text-brand" : "text-ink"}`}
       href={href}
     >
       {name}
