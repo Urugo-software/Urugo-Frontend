1391e64222383cfa2a6af5e032f0555966fdd929
Leon <irakaramale@gmail.com>
2026-08-22 14:17:29 +0300

    convert waiting list page to server component

--- app/(dashboard)/home-seeker/waiting-list/page.tsx
+++ app/(dashboard)/home-seeker/waiting-list/page.tsx
@@ -1,23 +1,9 @@
-"use client";
-
-import { useState } from "react";
-import { fullWaitingListData } from "@/data/waiting-list-data";
-import { WaitingListHeader } from "@/components/dashboard/home-seeker/waiting-list/WaitingListHeader";
-import { WaitingListInfoBanner } from "@/components/dashboard/home-seeker/waiting-list/WaitingListInfoBanner";
-import { WaitingListContainer } from "@/components/dashboard/home-seeker/waiting-list/WaitingListContainer";
+import { WaitingListWrapper } from "@/components/dashboard/home-seeker/waiting-list/WaitingListWrapper";
 
 export default function WaitingListPage() {
-  const [items, setItems] = useState(fullWaitingListData);
-
-  const handleLeave = (id: string) => {
-    setItems((prev) => prev.filter((item) => item.id !== id));
-  };
-
   return (
     <div className="flex-1 overflow-y-auto p-6 sm:p-8 md:p-10">
-      <WaitingListHeader />
-      <WaitingListInfoBanner />
-      <WaitingListContainer items={items} onLeave={handleLeave} />
+      <WaitingListWrapper />
     </div>
   );
 }
