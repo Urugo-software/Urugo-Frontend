eb7fb19e36e83176aefc7f24d5b84557b97e1e2f
Leon <irakaramale@gmail.com>
2026-08-22 12:32:48 +0300

    remove git warnings

--- components/dashboard/DashboardHeader.tsx
+++ components/dashboard/DashboardHeader.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Link from "next/link";
-import { Bell, Sparkles } from "lucide-react";
+import { Bell } from "lucide-react";
 import CustomButton from "@/components/shared/CustomButton";
 
 interface DashboardHeaderProps {
