2742a1d5d90c809573ed474157daec0210149eb5
Leon <irakaramale@gmail.com>
2026-08-27 00:50:05 +0300

    Update dashboard header

--- components/dashboard/DashboardHeader.tsx
+++ components/dashboard/DashboardHeader.tsx
@@ -1,8 +1,9 @@
 "use client";
 
 import Link from "next/link";
-import { Bell } from "lucide-react";
+import { Bell, House } from "lucide-react";
 import CustomButton from "@/components/shared/CustomButton";
+import MobileNavWrapper from "./home-seeker/mobile-nav/mobile-nav-wrapper";
 
 interface DashboardHeaderProps {
   title?: string;
@@ -10,15 +11,16 @@ interface DashboardHeaderProps {
 
 export function DashboardHeader({ title = "Dashboard" }: DashboardHeaderProps) {
   return (
-    <header className="sticky top-0 z-20 flex h-18 shrink-0 items-center justify-between border-b border-line  px-6 sm:px-8 backdrop-blur-md">
+    <header className="sticky top-0 z-20 flex h-18 shrink-0 items-center justify-between border-b border-line  px-4 sm:px-8 backdrop-blur-md">
       {/* Current Page Title */}
       <div className="flex items-center gap-3">
+        <MobileNavWrapper />
         <h1 className="text-xl  font-bold text-ink sm:text-2xl">{title}</h1>
       </div>
 
       {/* Header Actions & Routing */}
-      <div className="flex items-center gap-3">
-        <Link href="/">
+      <div className=" flex items-center gap-3">
+        <Link className="max-md:hidden" href="/">
           <CustomButton
             variant="light"
             className="flex justify-center min-w-fit  rounded-none"
@@ -29,12 +31,17 @@ export function DashboardHeader({ title = "Dashboard" }: DashboardHeaderProps) {
           </CustomButton>
         </Link>
 
+        <Link className="md:hidden " href="/">
+          <span className="block  " aria-label="home button">
+            <House className="size-5 text-ink " />
+          </span>
+        </Link>
         <Link
           href="/home-seeker/notifications"
           aria-label="Notifications"
-          className="relative flex size-9 shrink-0 items-center justify-center rounded-full border border-line transition hover:bg-surface cursor-pointer"
+          className="relative flex size-9 shrink-0 items-center justify-center rounded-full border border-line max-md:border-none transition hover:bg-surface cursor-pointer"
         >
-          <Bell className="size-4 text-ink" />
+          <Bell className="size-4 max-md:size-5  text-ink" />
           <span className="absolute top-1.5 right-1.5 size-2 rounded-full bg-red-600 animate-pulse" />
         </Link>
       </div>
