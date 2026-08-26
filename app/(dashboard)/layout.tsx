e503ae73c51e7999ddf71687985435a285159d8f
Leon <irakaramale@gmail.com>
2026-08-27 00:50:04 +0300

    Update dashboard layout

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -6,9 +6,11 @@ function layout({ children }: { children: ReactNode }) {
   return (
     <main className="flex min-h-0  h-[100dvh] w-screen overflow-hidden  font-sans text-ink antialiased">
       <DashboardSidebar currentRole="home_seeker" />
-      <section className="flex flex-1 flex-col h-full min-w-0 overflow-hidden">
+      <section className="relative flex flex-1 flex-col h-full min-w-0 overflow-hidden">
         <DashboardHeader />
-        <div className="flex flex-1 flex-col min-h-0 overflow-hidden">{children}</div>
+        <div className="flex flex-1 flex-col min-h-0 overflow-hidden">
+          {children}
+        </div>
       </section>
     </main>
   );
