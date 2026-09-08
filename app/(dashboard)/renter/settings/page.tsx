35f0c31c7a6f2f1b9f7880e531d14f1b93fdc3ab
Leon <irakaramale@gmail.com>
2026-09-08 15:02:56 +0300

    add renter settings page

---
+++ app/(dashboard)/renter/settings/page.tsx
@@ -0,0 +1,9 @@
+import { RenterSettingsView } from "@/components/dashboard/renter/settings/RenterSettingsView";
+
+export default function RenterSettingsPage() {
+  return (
+    <main className="flex flex-1 flex-col min-h-0 min-w-0 overflow-hidden bg-white h-full">
+      <RenterSettingsView />
+    </main>
+  );
+}
