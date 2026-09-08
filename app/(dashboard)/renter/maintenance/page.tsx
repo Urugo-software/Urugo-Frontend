1c3f3fd440592f29e634485b8242302558c942e2
Leon <irakaramale@gmail.com>
2026-09-08 15:02:54 +0300

    add renter maintenance page

---
+++ app/(dashboard)/renter/maintenance/page.tsx
@@ -0,0 +1,9 @@
+import { RenterMaintenanceView } from "@/components/dashboard/renter/maintenance/RenterMaintenanceView";
+
+export default function RenterMaintenancePage() {
+  return (
+    <main className="flex flex-1 flex-col min-h-0 min-w-0 overflow-hidden bg-white h-full">
+      <RenterMaintenanceView />
+    </main>
+  );
+}
