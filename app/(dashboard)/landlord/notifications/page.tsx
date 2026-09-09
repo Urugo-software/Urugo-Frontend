45032530d2d737b2f6a8553e7bb4f62efc00a9fc
Leon <irakaramale@gmail.com>
2026-09-10 02:13:01 +0300

    add landlord notifications page route

---
+++ app/(dashboard)/landlord/notifications/page.tsx
@@ -0,0 +1,9 @@
+import { LandlordNotificationsView } from "@/components/dashboard/landlord/notifications/LandlordNotificationsView";
+
+export default function LandlordNotificationsPage() {
+  return (
+    <main className="flex flex-1 flex-col min-h-0 min-w-0 overflow-hidden bg-white h-full">
+      <LandlordNotificationsView />
+    </main>
+  );
+}
