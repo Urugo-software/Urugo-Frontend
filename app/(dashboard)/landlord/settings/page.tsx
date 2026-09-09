ebf0e5af30df9dda79fd0303f494c66f3f858fe9
Leon <irakaramale@gmail.com>
2026-09-10 02:13:03 +0300

    add landlord settings page route

---
+++ app/(dashboard)/landlord/settings/page.tsx
@@ -0,0 +1,9 @@
+import { LandlordSettingsView } from "@/components/dashboard/landlord/settings/LandlordSettingsView";
+
+export default function LandlordSettingsPage() {
+  return (
+    <main className="flex flex-1 flex-col min-h-0 min-w-0 overflow-hidden bg-white h-full">
+      <LandlordSettingsView />
+    </main>
+  );
+}
