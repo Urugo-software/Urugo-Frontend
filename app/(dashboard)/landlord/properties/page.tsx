9bad3d1cc5b5ada4b10124e0750a14d8e2065bca
Leon <irakaramale@gmail.com>
2026-09-10 02:13:00 +0300

    add landlord properties page route

---
+++ app/(dashboard)/landlord/properties/page.tsx
@@ -0,0 +1,9 @@
+import { LandlordPropertiesView } from "@/components/dashboard/landlord/properties/LandlordPropertiesView";
+
+export default function LandlordPropertiesPage() {
+  return (
+    <main className="flex flex-1 flex-col min-h-0 min-w-0 overflow-hidden bg-white h-full">
+      <LandlordPropertiesView />
+    </main>
+  );
+}
