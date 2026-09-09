7b5e34db444e486163d51ab8ee3709c2f972c9de
Leon <irakaramale@gmail.com>
2026-09-10 02:10:40 +0300

    update landlord main overview page

--- app/(dashboard)/landlord/page.tsx
+++ app/(dashboard)/landlord/page.tsx
@@ -1,5 +1,10 @@
-function page() {
-  return <div>welcome to landlord dashbord</div>;
+import { LandlordOverviewDashboard } from "@/components/dashboard/landlord/overview/LandlordOverviewDashboard";
+
+export default function LandlordPage() {
+  return (
+    <main className="flex flex-1 flex-col min-h-0 min-w-0 overflow-hidden bg-white h-full">
+      <LandlordOverviewDashboard />
+    </main>
+  );
 }
 
-export default page;
