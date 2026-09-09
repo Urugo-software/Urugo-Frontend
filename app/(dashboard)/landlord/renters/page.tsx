6ef140a5ebcc393fb194ba7fe69d335a59396617
Leon <irakaramale@gmail.com>
2026-09-10 02:13:40 +0300

    add landlord renters page route

---
+++ app/(dashboard)/landlord/renters/page.tsx
@@ -0,0 +1,9 @@
+import { RentersListContainer } from "@/components/dashboard/landlord/renters/RentersListContainer";
+
+export default function LandlordRentersPage() {
+  return (
+    <main className="flex flex-1 flex-col min-h-0 min-w-0 overflow-hidden bg-white h-full">
+      <RentersListContainer />
+    </main>
+  );
+}
