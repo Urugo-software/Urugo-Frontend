9282e739077e64f995e9de14bda769ba298b7628
Leon <irakaramale@gmail.com>
2026-09-10 02:13:04 +0300

    add landlord help center page route

---
+++ app/(dashboard)/landlord/help-center/page.tsx
@@ -0,0 +1,9 @@
+import { LandlordHelpCenterView } from "@/components/dashboard/landlord/help-center/LandlordHelpCenterView";
+
+export default function LandlordHelpCenterPage() {
+  return (
+    <main className="flex flex-1 flex-col min-h-0 min-w-0 overflow-hidden bg-white h-full">
+      <LandlordHelpCenterView />
+    </main>
+  );
+}
